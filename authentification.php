2c02d180fc768d9acdf5248f691054bdc55e7a37
Moutar12 <63120298+Moutar12@users.noreply.github.com>
2020-04-11 17:28:20 +0100

    Add files via upload

---
+++ authentification.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width='', initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="connexion.css">
+</head>
+
+<body>
+  <img class="img" src="Images/logo-QuizzSA.png" alt="">
+<div class="header">
+   <p>Le plaisir de jouer</p>
+   </div>
+  
+
+    <div class="form">
+    <form action="" methodd="POST">
+    <div class="label">
+    <label>Login Form  &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;  X</label><br>
+    </div>
+    <input type="text" name="login" placeholder="Login" value=""><br><br>
+    <input type="password" name="motdepasse" placeholder="Password" value=""><br><br>
+    
+    <div class="input" >
+    <input type="submit" name="submit" value="Connexion" id="connexion">
+</div>
+<div class="lien">
+    <a href="inscription.php">S'incrire pour le jeu?</a>
+    </div>
+    <div class="icone1">
+        <img src="Images/Icones/ic-login.png" alt=""style="width:25px; height:30px;">
+</div>
+<div class="icone2">
+    <img src="Images/Icones/ic-password.png" alt="" style="width:25px; height:30px;">
+</div>
+    </form>
+    </div>
+</body>
+</html>
