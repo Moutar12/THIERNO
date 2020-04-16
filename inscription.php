b23a4274875d8ab4c503ede3102bf6982de934e8
Moutar12 <63120298+Moutar12@users.noreply.github.com>
2020-04-16 22:49:22 +0100

    Add files via upload

---
+++ inscription.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="header">
+<div class="logo"></div>
+    <div class="header-text">Le plaisir de jouer</div>
+</div>
+    <div class="content">
+<div class="contenue">
+<div class="couverture">
+<div class="entete">
+        <div class="quizz">CRÉER ET  PARAMÉRTER VOS QUIZZ</div>
+    </div>
+    <div class="conteneur_menu">
+<div class="menu">
+<div id="menu_vertical">
+<div class="img_avatar"><img src="./images/img-bg.jpg" alt="">
+    <div class="aaa">AAA</div>
+    <div class="bbb">BBB</div>
+</div>
+<ul class="navigation">
+ <li> <a href="#">Liste Questions</a></li>
+ <li><a href="#">Créer Admin</a></li> 
+  <li><a href="#">Liste Joueurs</a></li>
+  <li><a href="#">Créer Questions</a></li>
+  </ul>
+  </div>
+  <div class="img">
+      <img src="./Icones/ic-liste.png" alt="" class="liste_1">
+      <img src="./Icones/admin.png" alt="" class="admin">
+      <img src="./Icones/ic-liste.png" alt="" class="liste_2">
+      <img src="./Icones/ic-ajout.png" alt="" class="ajout">
+  </div>
+  <div class="formulaire">
+<div class="inscription">S'INCRIRE</div>
+<div class="propos">Pour proposer des quizz</div>
+<hr><br>
+<div class="form">
+    <form action="" method="post">
+        <label for="">Prénom</label><br>
+        <input type="text" name="prenom" id=""><br>
+        <label for="">Nom</label><br>
+        <input type="text" name="nom" id=""><br>
+        <label for="">Login</label><br>
+        <input type="text" name="login" id=""><br>
+        <label for="">Password</label><br>
+        <input type="password" name="pwd" id=""><br>
+        <label for="">Confirmer Password</label><br>
+        <input type="password" name="password" id=""><br><br>
+        <label for="" class="Avatar">Avatar</label><br>
+        <span class="btn btn-primary fileinput-button">
+    <span>Choisir un fichier</span>
+    <input type="file" name="files" multiple>
+    </span>
+        <button type="submit">Créer compte</button>
+       
+    <img src="./images/img-bg.jpg" class="img1" alt="">
+    <div class="avatar_admin">Avatar Admin</div>
+    </form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
---
+++ listedesQuestions.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="header">
+<div class="logo"></div>
+    <div class="header-text">Le plaisir de jouer</div>
+</div>
+    <div class="content">
+<div class="contenue">
+<div class="couverture">
+<div class="entete">
+        <div class="quizz">CRÉER ET  PARAMÉRTER VOS QUIZZ</div>
+    </div>
+    <div class="conteneur_menu">
+<div class="menu">
+<div id="menu_vertical">
+    <div class="img_avatar"><img src="./images/img-bg.jpg" alt="">
+    <div class="aaa">AAA</div>
+    <div class="bbb">BBB</div>
+</div>
+<ul class="navigation">
+ <li> <a href="#">Liste Questions</a></li>
+ <li><a href="#">Créer Admin</a></li> 
+  <li><a href="#">Liste Joueurs</a></li>
+  <li><a href="#">Créer Questions</a></li>
+  </ul>
+  </div>
+  <div class="img">
+      <img src="./Icones/ic-liste.png" alt="" class="liste_1">
+      <img src="./Icones/admin.png" alt="" class="admin">
+      <img src="./Icones/ic-liste.png" alt="" class="liste_2">
+      <img src="./Icones/ic-ajout.png" alt="" class="ajout">
+  </div>
+  <div class="mon_avatar">
+      
+  </div>
+  <div class="formulaire">
+
+<div class="form">
+    <form action="" method="post">
+    <div class="texte"> <label for="" class="nbre">Nbre de questions/jeux</label><input type="text" name="jeu" id="">
+    <div class="submit"><button type="submit">OK</button></div>
+    
+    </div>
+    <div class="Input">
+        <h3>1. Les langages web</h3>
+        <input type="checkbox" name="html" id="">
+        <label for="" class="label">HTML</label><br>
+        <input type="checkbox" name="R" id="">
+        <label for="" class="label">R</label><br>
+        <input type="checkbox" name="java" id="">
+        <label for="" class="label">JAVA</label><br>
+        <h3>2. D'ou vient le Corona</h3>
+        <input type="radio" name="chine" id="">
+        <label for="" class="label">Chine</label><br>
+        <input type="radio" name="italie" id="">
+        <label for="" class="label">Italie</label><br>
+        <h3>3. Quel terme déﬁnit langage qui s’adapte sur Androïd et sur Ios</h3>
+        <input type="text" name="text" id="">
+        <h3>4. Quelle est la première école de codage gratuite au Sénégal</h3>
+        <input type="radio" name="Simplon" id="">
+        <label for="" class="label">Simplon</label><br>
+        <input type="radio" name="orange" id="">
+        <label for="" class="label">Orange Digital Center</label><br>
+        <h3>5. Les précurseurs de la révolution digitale</h3>
+        <input type="radio" name="GAFAM" id="">
+        <label for="" class="label">GAFAM</label><br>
+        <input type="radio" name="CIA-FBI" id="">
+        <label for="" class="label">CIA-FBI</label><br>
+        </div>
+        <div class="button">
+            <button type="submit">Suivant</button>
+        </div>
+    </form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
